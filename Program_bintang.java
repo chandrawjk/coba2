20bc34568d09233fa7c524f93e3624afdf6657d0
chandrazack <chandra.shyarmizi@gmail.com>
2024-01-12 11:31:39 +0700

    tambah komentar

--- Program_bintang.java
+++ Program_bintang.java
@@ -11,7 +11,7 @@ public class Program_bintang {
 
     public static void main(String[] args) {
         int tinggi = 5; // Ubah tinggi sesuai kebutuhan
-
+//tes
         for (int i = 1; i < 5; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
